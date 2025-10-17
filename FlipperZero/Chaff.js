ad2fc3c2c789d095fb84afc4612561ac5c5f5575
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-10-16 21:51:44 -0500

    Add files via upload

--- FlipperZero/Chaff.js
+++ FlipperZero/Chaff.js
@@ -1,10 +1,9 @@
 let math = require("math");
 let storage = require("storage");
-let free = storage.fsInfo("/ext")["freeSpace"];
 print("CREATING CHAFF");
-let data = ""; for (let i = 0; i < math.pow(2,8); i++) data += "0";
+let data = ""; for (let i = 0; i < 2048; i++) data += "0";
 let file = storage.openFile("/ext/CHAFF", "w", "create_always");
-for (let i = 0; i < math.floor(free / math.pow(2,8)); i += 1)
+while (true)
 {
     file.write(data);
 }
